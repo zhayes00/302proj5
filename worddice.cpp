8b049746939e0ac8a63edbbaf74d085b4764eac8
Zackery Rodger Hayes <zhayes2@hydra28.eecs.utk.edu>
2021-11-08 13:05:50 -0500

    Addition of "weight" graph

--- worddice.cpp
+++ worddice.cpp
@@ -6,6 +6,15 @@ using namespace std;
 
 int main() {
 
+	//add classes from piazza?
+	//use edmonds-karp
+	//find max flow will solve
+	
+	//pieces:							|	"anticipated weight"
+	//	class & member function setup	|	medium?
+	//	edmonds-karp & BFS				|	heavy
+	//	input/output					|	light
+	//	
 
 
 }
